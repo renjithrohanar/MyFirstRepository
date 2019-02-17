900e3bb5b573277a492e2bdd41235663747d94b1
Renjith Rohan <renjithrohanar@gmail.com>
2019-02-16 21:43:27 -0600

    My first commit

---
+++ src/test/java/com/chc/checkmaven/MavenTest.java
@@ -0,0 +1,14 @@
+package com.chc.checkmaven;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+
+	@Test
+	public void mavenTest () {
+
+		System.out.println("Hello Maven World");
+		
+	}
+
+}
